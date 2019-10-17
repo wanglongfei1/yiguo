d67b251b10d6a89ff667d9427aa3a18e1edf7ac0
Lliushuo <ls960246936@163.com>
2019-10-17 14:12:57 +0800

    解决mint ui 按需引入修改babel配置文件

--- yiguo/babel.config.js
+++ yiguo/babel.config.js
@@ -1,5 +1,13 @@
+司法信息 陈琪 14:11:47
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  "plugins": [["component", 
+    {
+      "libraryName": "mint-ui",
+      "style": true
+    }
+  ]]
 }
+
